ff6cbc0648c1abe7b3b57b41aebddc7cb438b218
liuwei <liuwei001@zhongan.io>
2019-11-08 16:54:53 +0800

    feat:优化代码

--- src/create.js
+++ src/create.js
@@ -1,24 +1,35 @@
-const inquirer = require('inquirer');  
-const questions =[
-    {
-        type: 'list',
-        name: 'projectName',
-        message: "你想创建什么类型的项目",
-        choices:[
-            {name:'react'},
-            {name:'vue'},
-        ],
-        default: function() {
-            return 'react';
+const inquirer = require("inquirer");
+const download = require("download-git-repo");
+const ora = require("ora");
+
+const questions = [
+  {
+    type: "list",
+    name: "projectType",
+    message: "你想创建什么类型的项目",
+    choices: [{ name: "react" }, { name: "vue" }],
+    default: function() {
+      return "react";
+    }
+  }
+];
+module.exports = async projectName => {
+  inquirer.prompt(questions).then(function(answers) {
+    if (answers.projectType === "react") {
+      const spinner = ora().start();
+      spinner.color = "yellow";
+      spinner.text = "正在加载项目";
+      download(
+        "github:kinsliy/reactDev",
+        `${projectName}`,
+        { clone: true },
+        function(err) {
+            spinner.stop()
+          console.log(err ? "Error" : "Success");
+          process.exit(1);
         }
-    },
-
-
-]
-module.exports = async (projectName) => {
-
-inquirer.prompt(questions).then(function (answers) {  
-    console.log(answers)
-});
+      );
+    }
+    console.log(answers);
+  });
 };
-  
